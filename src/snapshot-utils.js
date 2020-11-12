ac3769425e0b1564348c3fdb2f46eb16272e2746
Clay Smith <smithclay@gmail.com>
2020-11-12 15:10:17 -0800

    add logic for service operations

--- src/snapshot-utils.js
+++ src/snapshot-utils.js
@@ -47,11 +47,25 @@ const snapshotServiceStats = (snapshot) => {
     }
     const services = [...new Set(snapshot.data.attributes.exemplars.map(e => e['service-name']))]
 
+    // get average duration + error pct for an array of spans
+    const spanStats = (spans) => {
+        const avgDurationMs = spans.reduce((v, e) => v + e['duration-micros'], 0) / (spans.length*1000)
+        const errorPct = spans.reduce((v, e) => v + (e['has-error'] ? 1 : 0), 0) / spans.length
+        return { avgDurationMs, errorPct }
+    }
+
     return services.reduce((obj, s) => {
         const exemplars = snapshot.data.attributes.exemplars.filter(e => e['service-name'] === s)
-        const avgDurationMs = exemplars.reduce((v, e) => v + e['duration-micros'], 0) / (exemplars.length*1000)
-        const errorPct = exemplars.reduce((v, e) => v + (e['has-error'] ? 1 : 0), 0) / exemplars.length
-        obj[s] = { exemplars, avgDurationMs, errorPct }
+        const operationNames = [...new Set(exemplars.map(e => e['span-name']))]
+        const { avgDurationMs, errorPct } = spanStats(exemplars)
+
+        // stats group by operation name per service
+        const operations = operationNames.reduce((obj, o) => {
+            obj[o] = spanStats(exemplars.filter(e => e['span-name'] === o))
+            return obj
+        }, {})
+
+        obj[s] = { exemplars, operations, operationNames, avgDurationMs, errorPct }
         return obj
     }, {})
 }
@@ -94,17 +108,50 @@ const diagramDiff = (beforeDiagram, afterDiagram) => {
 const snapshotDiff = (beforeSnapshot, afterSnapshot) => {
     const beforeStats = snapshotServiceStats(beforeSnapshot)
     const afterStats = snapshotServiceStats(afterSnapshot)
+
     const avgDurationMs = Object.keys(afterStats).reduce((obj, s) => {
         const diff = (afterStats[s].avgDurationMs - beforeStats[s].avgDurationMs)
+
+        // avg duration change by operation
+        const operations = Object.keys(afterStats[s].operations).reduce((obj, o) => {
+            if (!afterStats[s].operations[o] || !beforeStats[s].operations[o]) {
+                obj[o] = { diff : 0, pct : 0 }
+                return obj
+            }
+            const diff = afterStats[s].operations[o].avgDurationMs -
+                beforeStats[s].operations[o].avgDurationMs
+            obj[o] = { diff, pct : (diff/beforeStats[s].operations[o].avgDurationMs) }
+            return obj
+        }, {})
+
+        const deleted_operations = difference(beforeStats[s].operationNames, afterStats[s].operationNames)
+        const added_operations = difference(afterStats[s].operationNames, beforeStats[s].operationNames)
+
         obj[s] = {
+            operations,
             diff,
+            added_operations,
+            deleted_operations,
             pct : (diff/beforeStats[s].avgDurationMs)
         }
         return obj
     }, {})
-    const errorPct = Object.keys(afterStats).reduce((obj, s) => { 
+    const errorPct = Object.keys(afterStats).reduce((obj, s) => {
         const diff = (afterStats[s].errorPct - beforeStats[s].errorPct)
+
+        // error change by operation
+        const operations = Object.keys(afterStats[s].operations).reduce((obj, o) => {
+            if (!afterStats[s].operations[o] || !beforeStats[s].operations[o]) {
+                obj[o] = { diff : 0, pct : 0 }
+                return obj
+            }
+            const diff = afterStats[s].operations[o].error -
+                beforeStats[s].operations[o].error
+            obj[o] = { diff, pct : diff }
+            return obj
+        }, {})
         obj[s] = {
+            operations,
             diff,
             pct : diff
         }
